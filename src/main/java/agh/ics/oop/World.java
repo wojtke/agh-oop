2c8f020d98eabc4fbb6cd46438e242a605d452d0
Wojciech Jasiński <woj.jasinski@gmail.com>
2021-10-21 12:06:56 +0200

    lab2 bez testow jeszcze

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -2,11 +2,11 @@ package agh.ics.oop;
 
 public class World {
     public static void main(String[] args) {
-        System.out.println("program wystartowal");
-
-        run(args_to_enum(args));
-
-        System.out.println("\nprogram zakonczyl dzialanie");
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
     }
 
     public static void run(Direction[] dirs) {
